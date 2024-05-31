3cfa424996491f42e30c90cd5ddb2a5f0724afc4
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2024-05-31 09:39:57 +0100

    Add endpoint to patch a record in the hawk stub

---
+++ app/uk/gov/hmrc/tradergoodsprofilestestsupport/controllers/RecordsController.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tradergoodsprofilestestsupport.controllers
+
+import play.api.mvc.{Action, ControllerComponents}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendBaseController
+import uk.gov.hmrc.tradergoodsprofilestestsupport.connectors.RecordsConnector
+import uk.gov.hmrc.tradergoodsprofilestestsupport.models.{GoodsItemPatch, GoodsItemPatchRequest}
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class RecordsController @Inject()(
+                                   override val controllerComponents: ControllerComponents,
+                                   connector: RecordsConnector
+                                 )(implicit ec: ExecutionContext) extends BackendBaseController {
+
+  def patch(eori: String, recordId: String): Action[GoodsItemPatchRequest] = Action(parse.json[GoodsItemPatchRequest]).async { implicit request =>
+    val patch = GoodsItemPatch.fromPatchRequest(eori, recordId, request.body)
+
+    connector.patch(patch)
+      .map(_ => Ok)
+  }
+}
---
+++ app/uk/gov/hmrc/tradergoodsprofilestestsupport/models/GoodsItemPatch.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tradergoodsprofilestestsupport.models
+
+import play.api.libs.json.{Json, OFormat}
+
+final case class GoodsItemPatch(
+                                 eori: String,
+                                 recordId: String,
+                                 accreditationStatus: Option[AccreditationStatus],
+                                 version: Option[Int],
+                                 active: Option[Boolean],
+                                 locked: Option[Boolean],
+                                 toReview: Option[Boolean],
+                                 reviewReason: Option[String],
+                                 declarable: Option[String]
+                               )
+
+object GoodsItemPatch {
+
+  def fromPatchRequest(eori: String, recordId: String, patchRequest: GoodsItemPatchRequest): GoodsItemPatch =
+    GoodsItemPatch(
+      eori = eori,
+      recordId = recordId,
+      accreditationStatus = patchRequest.accreditationStatus,
+      version = patchRequest.version,
+      active = patchRequest.active,
+      locked = patchRequest.locked,
+      toReview = patchRequest.toReview,
+      reviewReason = patchRequest.reviewReason,
+      declarable = patchRequest.declarable
+    )
+
+  implicit lazy val format: OFormat[GoodsItemPatch] = Json.format
+}
